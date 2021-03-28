d1e995fe9363f30d6abdc3b317f5634008e081c9
Christopher Arbelo <christopherarbelo@gmail.com>
2021-03-28 13:56:46 -0400

    Add form to select a flight to book
    flights.html.erb was changed to have a form to book flights from the results of the search. flights_controller.rb included a passenger_count instance variable because available_flights which queries from the Flight table doesn't have passenger count information. Passenger count information is inputted from the user and thus needs a separate instance variable to pass to the view.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,9 @@
 class FlightsController < ApplicationController
   def flights
-    @airport_options = Airport.all.map { |airport| [airport.code, airport.id]}
+    @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
     @available_dates = Flight.all.available_dates.map.with_index
     @available_flights = Flight.includes(:origin, :destination).where(search_params).to_a
+    @passenger_count = params.permit(:passengers)
   end
 
   private
