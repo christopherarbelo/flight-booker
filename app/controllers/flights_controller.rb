cf72c670a99a8e074d721c4ae4c9d4ea8cf65be9
Christopher Arbelo <christopherarbelo@gmail.com>
2021-03-28 19:07:36 -0400

    Flights page only shows available flights when searched and available flights is moved to a partial.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,12 +2,19 @@ class FlightsController < ApplicationController
   def flights
     @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
     @available_dates = Flight.all.available_dates.map.with_index
-    @available_flights = Flight.includes(:origin, :destination).where(search_params).to_a
-    @passenger_count = params.permit(:passengers)
+    @search = legal_flight_search
+    if @search
+      @available_flights = Flight.includes(:origin, :destination).where(search_params).to_a
+      @passenger_count = params[:passengers]
+    end
   end
 
   private
   def search_params
     params.permit(:origin_id, :destination_id, :date)
+  end
+
+  def legal_flight_search
+    !(params[:origin_id].blank? && params[:destination_id].blank? && params[:date].blank?)
   end
 end
